d63a62af4a4f6d493adff4cacc3744e00ecf3ca4
Alice Lara <hello@alicelara.ca>
2013-06-24 13:24:13 -0400

    McHammer Bot - UI

---
+++ html/js/uimove.js
@@ -0,0 +1,10 @@
+//$('#draggable').draggable({
+//    drop: function( event, ui ) {
+//
+//         position of the draggable minus position of the droppable
+//         relative to the document
+//        var $newPosX = ui.offset.left - $(this).offset().left;
+//        var $newPosY = ui.offset.top - $(this).offset().top;
+//
+//    }
+//});
